533201d743edb66c69b24953bfba70b412408c04
Immady <ahmadali63288@gmail.com>
2026-05-19 19:21:43 +0530

    simple mock implementation

---
+++ internal/service/link_service_test.go
@@ -0,0 +1,31 @@
+package service
+
+import (
+	"errors"
+	"url_saver/internal/models"
+)
+
+// 1. Simple Mock Implementation
+type MockStore struct {
+	shouldFail bool // Toggle this if you want to mock database errors
+}
+
+func (m *MockStore) Save(data models.Link) (models.Link, error) {
+	if m.shouldFail {
+		return models.Link{}, errors.New("db error")
+	}
+	return data, nil
+}
+
+func (m *MockStore) GetAll() ([]models.Link, error) {
+	return []models.Link{{ID: 1, Title: "Google", Link: "https://google.com"}}, nil
+}
+
+func (m *MockStore) Update(upId int64, data models.Link) (models.Link, error) {
+	data.ID = upId
+	return data, nil
+}
+
+func (m *MockStore) Delete(delId int64) error {
+	return nil
+}
